31f2bfabc54e5d5b1d9f18b03259491321ce2a2b
Catislin <caitlin.estrellita@gmail.com>
2017-09-19 13:59:35 -0700

    added form template and put method

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const express = require('express');
-const app = express();
-const hb = require('express-handlebars');
-const mongoose = require('mongoose');
+var express = require('express');
+var app = express();
+var hb = require('express-handlebars');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var methodOverride = require('method-override');
 mongoose.connect('mongodb://localhost/rotten-potatoes');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
+
+var Review = mongoose.model('Review', {
+  title: String,
+  movieTitle: String,
+  description: String,
+  rating: Number
+});
 
 app.engine('handlebars', hb({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
-
-var Review = mongoose.model('Review', {
-  title: String
-});
 
 app.get('/', function (req, res) {
   Review.find(function(err, reviews) {
@@ -19,6 +26,32 @@ app.get('/', function (req, res) {
 
 app.get('/reviews/new', function (req, res) {
   res.render('reviews-new', {});
+});
+
+app.get('/reviews/:id', function (req, res) {
+  Review.findById(req.params.id).exec(function (err, review) {
+    res.render('reviews-show', {review: review});
+  });
+});
+
+app.get('/reviews/:id/edit', function (req, res) {
+  Review.findById(req.params.id, function (err, review) {
+    res.render('reviews-edit', {review: review});
+  });
+});
+
+app.put('/reviews/:id', function (req, res) {
+  console.log("trying to put");
+  Review.findByIdAndUpdate(req.params.id,  req.body, function(err, review) {
+    console.log("find by id and update running");
+    res.redirect('/reviews/' + review._id);
+  });
+});
+
+app.post('/reviews', function (req, res) {
+  Review.create(req.body, function(err, rv) {
+    res.redirect('/reviews/' + rv._id);
+  })
 });
 
 app.listen(3000, function() {
